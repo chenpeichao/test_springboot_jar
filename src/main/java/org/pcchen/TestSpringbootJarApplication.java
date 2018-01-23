2e2ea6152d11829273fcf3fb5621b1e173fdc605
chenpeichao <chenpeichao@pdmi.cn>
2018-01-23 22:34:56 +0800

    springboot项目搭建

---
+++ src/main/java/org/pcchen/TestSpringbootJarApplication.java
@@ -0,0 +1,13 @@
+package org.pcchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class TestSpringbootJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringbootJarApplication.class, args);
+	}
+}
---
+++ src/main/java/org/pcchen/web/UserController.java
@@ -0,0 +1,20 @@
+package org.pcchen.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户controller
+ * Created by ceek on 2018-01-23 22:00.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/find")
+    public String find() {
+        return "wojiushiwo";
+    }
+}
