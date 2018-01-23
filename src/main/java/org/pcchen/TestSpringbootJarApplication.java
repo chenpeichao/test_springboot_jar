8de897906dfa2062b17babe6dc2e4fde4c1a7dd8
chenpeichao <chenpeichao@pdmi.cn>
2018-01-23 23:10:18 +0800

    熟悉控制台不用答应springboot关键字

--- src/main/java/org/pcchen/TestSpringbootJarApplication.java
+++ src/main/java/org/pcchen/TestSpringbootJarApplication.java
@@ -1,13 +1,19 @@
 package org.pcchen;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TestSpringbootJarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestSpringbootJarApplication.class, args);
+//		SpringApplication.run(TestSpringbootJarApplication.class, args);
+
+        //此种方式可以使控制台不用显示springboot这几个字
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(TestSpringbootJarApplication.class);
+		//修改Banner的模式为OFF
+		builder.bannerMode(Banner.Mode.OFF).run(args);
 	}
 }
