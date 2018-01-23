98816064a61d0823830da4e36651d18c434b519a
cpc_geek <chenpeichao@pdmi.cn>
2018-01-23 23:12:40 +0800

    代码优化

--- src/main/java/org/pcchen/TestSpringbootJarApplication.java
+++ src/main/java/org/pcchen/TestSpringbootJarApplication.java
@@ -11,9 +11,11 @@ public class TestSpringbootJarApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(TestSpringbootJarApplication.class, args);
 
-        //此种方式可以使控制台不用显示springboot这几个字
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(TestSpringbootJarApplication.class);
-		//修改Banner的模式为OFF
-		builder.bannerMode(Banner.Mode.OFF).run(args);
+//        //此种方式可以使控制台不用显示springboot这几个字
+//		SpringApplicationBuilder builder = new SpringApplicationBuilder(TestSpringbootJarApplication.class);
+//		//修改Banner的模式为OFF
+//		builder.bannerMode(Banner.Mode.OFF).run(args);
+		SpringApplication springApplication = new SpringApplication(TestSpringbootJarApplication.class);
+		springApplication.run(args);
 	}
 }
