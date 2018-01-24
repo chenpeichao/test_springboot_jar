68c460784e239043b299747d8d060c4458c63380
chenpeichao <chenpeichao@pdmi.cn>
2018-01-24 09:00:07 +0800

    springboot启动类代码优化

--- src/main/java/org/pcchen/TestSpringbootJarApplication.java
+++ src/main/java/org/pcchen/TestSpringbootJarApplication.java
@@ -1,9 +1,7 @@
 package org.pcchen;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TestSpringbootJarApplication {
